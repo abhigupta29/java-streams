ff399f306df951c2313f04c7317c726ffca9d305
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-10-05 00:51:29 +0530

    Lecture3 - min,max and comparator

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -12,12 +12,21 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    Integer min = numbers.stream()
+            .min(Comparator.naturalOrder())
+            .get();
 
+    System.out.println(min);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer max = numbers.stream()
+            .max(Comparator.naturalOrder())
+            .get();
+
+    System.out.println(max);
   }
 }
