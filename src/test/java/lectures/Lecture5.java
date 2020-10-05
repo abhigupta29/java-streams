0c97ddad6e20d06f53ea328bd5003e5d6c132db4
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-10-05 22:12:35 +0530

    Lecture5 - filter and map

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -18,20 +18,35 @@ public class Lecture5 {
   @Test
   public void understandingFilter() throws Exception {
     ImmutableList<Car> cars = MockData.getCars();
+    List<Car> carsFiltered = cars.stream()
+            .filter(car -> car.getPrice() < 10000)
+            .collect(Collectors.toList());
 
+    carsFiltered.forEach(System.out::println);
+    System.out.println(carsFiltered.size());
   }
 
   @Test
   public void ourFirstMapping() throws Exception {
     // transform from one data type to another
     List<Person> people = MockData.getPeople();
+    List<PersonDTO> personDTOS = people.stream()
+            .map(person -> new PersonDTO(person.getId(), person.getFirstName(), person.getAge()))
+            .collect(Collectors.toList());
 
+    personDTOS.forEach(System.out::println);
   }
 
   @Test
   public void averageCarPrice() throws Exception {
     // calculate average of car prices
+    ImmutableList<Car> cars = MockData.getCars();
+    double averagePrice = cars.stream()
+            .mapToDouble(car -> car.getPrice())
+            .average()
+            .orElse(0);
 
+    System.out.println(averagePrice);
   }
 
   @Test
