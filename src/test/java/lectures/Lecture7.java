96e8cc661d4acb3123d21551b108c0340ed010c1
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-10-05 22:55:36 +0530

    Lecture7 - pulling statistics out of collections

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -16,49 +16,72 @@ public class Lecture7 {
 
   @Test
   public void count() throws Exception {
+    long femaleCount = MockData.getPeople()
+            .stream()
+            .filter(person -> person.getGender().equalsIgnoreCase("female"))
+            .count();
 
+    System.out.println(femaleCount);
   }
 
   @Test
   public void min() throws Exception {
+    double min = MockData.getCars()
+            .stream()
+            .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+            .mapToDouble(car -> car.getPrice())
+            .min()
+            .orElse(0);
 
+    System.out.println(min);
   }
 
   @Test
   public void max() throws Exception {
+    double max = MockData.getCars()
+            .stream()
+            .filter(car -> car.getColor().equalsIgnoreCase("yellow"))
+            .mapToDouble(car -> car.getPrice())
+            .max()
+            .orElse(0);
 
+    System.out.println(max);
   }
 
 
   @Test
   public void average() throws Exception {
     List<Car> cars = MockData.getCars();
+    double average = cars.stream()
+            .mapToDouble(car -> car.getPrice())
+            .average()
+            .orElse(0);
 
+    System.out.println(average);
   }
 
   @Test
   public void sum() throws Exception {
     List<Car> cars = MockData.getCars();
     double sum = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .sum();
+            .mapToDouble(car -> car.getPrice())
+            .sum();
     BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
     System.out.println(sum);
     System.out.println(bigDecimalSum);
-
   }
 
   @Test
   public void statistics() throws Exception {
     List<Car> cars = MockData.getCars();
     DoubleSummaryStatistics statistics = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .summaryStatistics();
+            .mapToDouble(car -> car.getPrice())
+            .summaryStatistics();
     System.out.println(statistics);
-    System.out.println(statistics.getAverage());
-    System.out.println(statistics.getCount());
     System.out.println(statistics.getMax());
     System.out.println(statistics.getMin());
+    System.out.println(statistics.getAverage());
+    System.out.println(statistics.getCount());
     System.out.println(statistics.getSum());
   }
 
