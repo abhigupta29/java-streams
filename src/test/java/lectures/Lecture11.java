6fd1d4a7ad13f4fbbf4eced7ce233b1d99cd441c
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-10-05 23:50:49 +0530

    Lecture11 - Joining strings

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -11,12 +11,22 @@ public class Lecture11 {
   @Test
   public void joiningStrings() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
+    String join = "";
+    for (int i=0; i < names.size(); i++) {
+      join = join + names.get(i);
+      if(i != names.size()-1) {
+        join = join + ",";
+      }
+    }
+    System.out.println(join);
   }
 
   @Test
   public void joiningStringsWithStream() throws Exception {
     List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    String join = names.stream()
+            .collect(Collectors.joining(","));
 
+    System.out.println(join);
   }
 }
