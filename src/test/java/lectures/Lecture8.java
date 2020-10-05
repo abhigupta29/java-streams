daaa20d166066d088e1b103d7a6f7aaa03631dc4
Abhishek Gupta <gupta.abhishek2996@gmail.com>
2020-10-05 23:13:27 +0530

    Lecture8 - grouping

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -16,7 +16,14 @@ public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+    Map<String, List<Car>> groupByMake = MockData.getCars()
+            .stream()
+            .collect(Collectors.groupingBy(car -> car.getMake()));
 
+    groupByMake.forEach((make, cars) -> {
+      System.out.println(make);
+      cars.forEach(System.out::println);
+    });
   }
 
   @Test
@@ -34,6 +41,10 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> counting = names.stream()
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+    counting.forEach((name, count) -> System.out.println(name + " > "+ count));
 
   }
 
